33d0d6af99a29c841dfa8db5537ce581ae804a01
riswanmsm <msmr1986@gmail.com>
2026-03-27 09:34:24 +0530

    url issues fixed for sub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://riswanmsm.github.io',
-  base: '/portfolio',
+  base: '/portfolio/',
   vite: {
     plugins: [tailwindcss()]
   },
